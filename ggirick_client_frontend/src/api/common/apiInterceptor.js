101e8801aa6c5ea7425feb46bcd525c143e6e739
Ju-S <wntjd0643@gmail.com>
2025-10-17 17:43:34 +0900

    react단에서 interceptor에 임시 아이디 'EMP003'토큰 발급받아서 인증받는 과정 적용.

--- ggirick_client_frontend/src/api/common/apiInterceptor.js
+++ ggirick_client_frontend/src/api/common/apiInterceptor.js
@@ -5,12 +5,15 @@ const BASE_URL = "http://10.5.5.7:8081";
 const api = axios.create();
 
 api.interceptors.request.use(
-    (config) => {
+    async (config) => {
         config.baseURL = BASE_URL;
         config.withCredentials = true;
-        // config.headers = {
-        //         Authorization: `Bearer ${token}` // JWT 강 컨베션
-        //     }
+
+        const resp = await axios.get(BASE_URL + "/testLogin")
+        config.headers = {
+            Authorization: `Bearer ${resp.data}` // JWT 강 컨베션
+        }
+        console.log(config.headers.Authorization);
         return config;
     },
     (error) => {
