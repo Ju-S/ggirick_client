a781f2f8e12f6adb59a23ad31be59ebb9b2466d8
Ju-S <wntjd0643@gmail.com>
2025-10-21 13:46:43 +0900

    FileUtil 작성. 간단한 파일 업로드, 다운로드 가능.

---
+++ ggirick_client_backend/src/main/java/com/kedu/ggirick_client_backend/utils/FileUtil.java
@@ -0,0 +1,45 @@
+package com.kedu.ggirick_client_backend.utils;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.kedu.ggirick_client_backend.dto.board.BoardFileDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class FileUtil {
+
+    private final Storage storage;
+
+    @Value("${spring.cloud.gcp.storage.bucket}")
+    private String bucketName;
+
+    // 파일 업로드(실제 저장된 파일명을 반환
+    public String fileUpload(String oriName, MultipartFile file) throws Exception {
+        String sysName = UUID.randomUUID() + "_" + oriName;
+        BlobInfo blobInfo =
+                BlobInfo.newBuilder(BlobId.of(bucketName, sysName))
+                        .setContentType(file.getContentType())
+                        .build();
+
+        try (InputStream is = file.getInputStream()) {
+            storage.createFrom(blobInfo, is);
+        }
+
+        return sysName;
+    }
+
+    // 파일 다운로드
+    public byte[] fileDownload(String sysName) {
+        Blob blob = storage.get(bucketName, sysName);
+        return blob.getContent();
+    }
+}
